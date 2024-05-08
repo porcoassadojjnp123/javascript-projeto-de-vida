5386b0b98b16bf942333a79f8f7bd8ef51f75886
porcoassadojjnp123 <107262542+porcoassadojjnp123@users.noreply.github.com>
2024-05-08 11:16:26 -0300

    08/05

--- main.js
+++ main.js
@@ -14,9 +14,18 @@ textos[i].classList.add("ativo");
 }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-06-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-05T00:00:00");
 
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
+
+function atualizaCronometo(){
+    for(let i= 0; i<contadores.length;i++){
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+}
+}
 
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
@@ -28,5 +37,15 @@ let dias = Math.floor(horas / 24);
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
+if(tempoFinal>0){
 return dias + "dias" + horas + "horas" + minutos +"minutos" + segundos + "segundos";
 }
+else{
+    return"prazo Finalizado"
+}
+}
+function comecaCrometro(){
+    atualizaCronometo();
+    setInterval(atualizaCronometo, 100)
+}
+//comecaCronometro();
