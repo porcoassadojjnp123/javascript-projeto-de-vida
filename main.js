92656f1f9e1a9cec52a632af0a0d2e46d4f9360a
porcoassadojjnp123 <107262542+porcoassadojjnp123@users.noreply.github.com>
2024-06-12 10:32:30 -0300

    12/06/2024

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function atualizaCronometro(){
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo - tempoAtual;
-let segundos = Math.floor(tempoAtual / 1000);
+let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos /  60);
 let dias = Math.floor(horas / 24);
